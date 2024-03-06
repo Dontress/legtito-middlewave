621a52ad4b26ba447812c9a7c18b100cf9389c16
Petr Šíma <petr@legito.com>
2024-03-06 17:19:21 +0100

    added routes for testing separate connections

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -12,6 +12,8 @@ const router = express.Router();
 
 router.route('/health').get((req, res) => res.send('Server is up!'));
 router.route('/test-connections').get(testSharepointConnection, testLegitoConnection, (req, res) => res.send('All connections are UP!'));
+router.route('/test-legito-connection').get(testLegitoConnection, (req, res) => res.send('Connection successful'));
+router.route('/test-sharepoint-connection').get(testSharepointConnection, (req, res) => res.send('Connection successful'));
 
 router.use('/connection', ConnectionRoute);
 router.use('/push-api', PushApi);
